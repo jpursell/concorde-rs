861511459b685e363096f2951786b62d0039a10e
Jeff Pursell <jpursell@gmail.com>
2024-06-13 05:57:03 -0400

    Add cc build

--- build.rs
+++ build.rs
@@ -1,16 +1,30 @@
-use std::env;
-use std::process::Command;
+// use std::env;
+// use std::process::Command;
+
+use glob::glob;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
+    // let out_dir = env::var("OUT_DIR").unwrap();
 
-    Command::new("make")
-        .env("OUT_DIR", &out_dir)
-        .args(["-C", "src/concorde"])
-        .output()
-        .expect("Failed to make");
+    // Command::new("make")
+    //     .env("OUT_DIR", &out_dir)
+    //     .args(["-C", "src/concorde"])
+    //     .output()
+    //     .expect("Failed to make");
 
-    println!("cargo:rustc-link-search=native={}", out_dir);
-    println!("cargo:rustc-link-lib=static=concorde");
-    println!("cargo:rerun-if-changed=src/concorde/*");
+    // println!("cargo:rustc-link-search=native={}", out_dir);
+    // println!("cargo:rustc-link-lib=static=concorde");
+    // println!("cargo:rerun-if-changed=src/concorde/*");
+
+
+    let srcs = glob("./src/concorde/**/*.c").unwrap().into_iter().map(|e| e.unwrap().display().to_string()).collect::<Vec<_>>();
+    let heads = glob("./src/concorde/**/*.h").unwrap().into_iter().map(|e| e.unwrap().display().to_string()).collect::<Vec<_>>();
+    cc::Build::new()
+        .files(&srcs)
+        .include("./src/concorde/INCLUDE/")
+        .compile("_concorde");
+
+    for path in srcs.iter().chain(heads.iter()) {
+        println!("cargo:rerun-if-changed={}", path);
+    }
 }
